562ac15b09528b568f88d3e557355708b2a8a249
David Nelson <thedoorman27@yahoo.com>
2023-01-06 08:05:12 -0800

    Week 1 Prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        bool pass = true;
+        Console.WriteLine("What is your percentage grade?");
+        string input = Console.ReadLine();
+        float percentage = float.Parse(input);
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (percentage < 70)
+        {
+            pass = false;
+        }
+        if (pass)
+        {
+            Console.WriteLine($"Your grade is: {letter}. Congratulations, you passed!");
+        }
+        else 
+        {
+            Console.WriteLine($"Your grade is: {letter}. Sorry, you did not pass. Please try again.");
+        }
     }
 }
