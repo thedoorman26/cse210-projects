6f8baee91647b7c1cc7bcec3167be3772fb28368
David Nelson <thedoorman27@yahoo.com>
2023-03-01 07:38:19 -0800

    Polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,91 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 3);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 6);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+    }
+}
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectangle(string color, double length, double width) : base (color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
     }
 }
